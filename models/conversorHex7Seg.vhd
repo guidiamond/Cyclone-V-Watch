c6664dd9d3d5a13c1026f5c0732858e558c86a4e
guidiamond <guilherme.batista770@gmail.com>
2020-10-20 12:36:37 -0300

    conversor removed unused params

--- models/conversorHex7Seg.vhd
+++ models/conversorHex7Seg.vhd
@@ -2,54 +2,53 @@ library IEEE;
 use ieee.std_logic_1164.all;
 
 entity conversorHex7Seg is
-    port
-    (
-        -- Input ports
-        dadoHex : in  std_logic_vector(3 downto 0);
-        apaga   : in  std_logic := '0';
-        negativo : in  std_logic := '0';
-        overFlow : in  std_logic := '0';
-        -- Output ports
-        saida7seg : out std_logic_vector(6 downto 0)  -- := (others => '1')
-    );
+  port
+  (
+      -- Input ports
+    Clk, enable     : in std_logic;
+    dadoHex : in  std_logic_vector(3 downto 0);
+      -- Output ports
+    saida7seg : out std_logic_vector(6 downto 0)  -- := (others => '1')
+  );
 end entity;
 
 architecture comportamento of conversorHex7Seg is
-   --
-   --       0
-   --      ---
-   --     |   |
-   --    5|   |1
-   --     | 6 |
-   --      ---
-   --     |   |
-   --    4|   |2
-   --     |   |
-   --      ---
-   --       3
-   --
-    signal rascSaida7seg: std_logic_vector(6 downto 0);
+    --
+    --       0
+    --      ---
+    --     |   |
+    --    5|   |1
+    --     | 6 |
+    --      ---
+    --     |   |
+    --    4|   |2
+    --     |   |
+    --      ---
+    --       3
+    --
+  signal rascSaida7seg: std_logic_vector(6 downto 0);
 begin
-    rascSaida7seg <=    "1000000" when dadoHex="0000" else ---0
-                            "1111001" when dadoHex="0001" else ---1
-                            "0100100" when dadoHex="0010" else ---2
-                            "0110000" when dadoHex="0011" else ---3
-                            "0011001" when dadoHex="0100" else ---4
-                            "0010010" when dadoHex="0101" else ---5
-                            "0000010" when dadoHex="0110" else ---6
-                            "1111000" when dadoHex="0111" else ---7
-                            "0000000" when dadoHex="1000" else ---8
-                            "0010000" when dadoHex="1001" else ---9
-                            "0001000" when dadoHex="1010" else ---A
-                            "0000011" when dadoHex="1011" else ---B
-                            "1000110" when dadoHex="1100" else ---C
-                            "0100001" when dadoHex="1101" else ---D
-                            "0000110" when dadoHex="1110" else ---E
-                            "0001110" when dadoHex="1111" else ---F
-                            "1111111"; -- Apaga todos segmentos.
+  rascSaida7seg <=        "1000000" when dadoHex=x"0" else ---0
+                          "1111001" when dadoHex=x"1" else ---1
+                          "0100100" when dadoHex=x"2" else ---2
+                          "0110000" when dadoHex=x"3" else ---3
+                          "0011001" when dadoHex=x"4" else ---4
+                          "0010010" when dadoHex=x"5" else ---5
+                          "0000010" when dadoHex=x"6" else ---6
+                          "1111000" when dadoHex=x"7" else ---7
+                          "0000000" when dadoHex=x"8" else ---8
+                          "0010000" when dadoHex=x"9" else ---9
+                          "0001000" when dadoHex=x"a" else ---A
+                          "0000011" when dadoHex=x"b" else ---P
+                          "1111111"; -- Delete all segments
 
-    saida7seg <=     "1100010" when (overFlow='1') else
-                            "1111111" when (apaga='1' and negativo='0') else
-                            "0111111" when (apaga='0' and negativo='1') else
-                            rascSaida7seg;
+  process (Clk) is
+  begin
+    if rising_edge(Clk) then
+      if (enable = '1') then
+        saida7seg <= rascSaida7seg;
+      end if;
+    end if;
+  end process;
+
 end architecture;
